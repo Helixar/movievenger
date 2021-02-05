f3b1048c0539045c2f255e9af09db09d0c63c75c
Astroalex <alexis31.balthazard@gmail.com>
2021-02-05 13:38:11 +0100

    Remove unused space and preview changes, login too

--- src/views/loginPage.php
+++ src/views/loginPage.php
@@ -2,34 +2,134 @@
 <html lang="fr">
 
 <head>
-    <meta charset="utf-8" />
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movie vengers</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <title>Movie Venger</title>
 </head>
 
-<body data-spy="scroll" data-offset="75">
+<body>
+    <!--HEADER--->
+    <div class="container-fluid p-4 header">
+        <h1 class="text-center text-light"><a href="/" class="nav-link">MOVIE VENGER</a></h1>
+    </div>
+    <!---->
+    <!--MENU-->
+    <div class="container">
+        <nav class="container navbar navbar-expand-lg">
+            <!--menu mobile-->
+            <div class="pos-f-t menu">
+                <div class="collapse" id="navbarToggleExternalContent">
+                    <ul class="navbar-nav">
+                        <li class="text-white nav-item active">
+                            <a href="/" class="nav-link">Accueil</a>
+                        </li>
+                        <li class=" text-white nav-item active">
+                            <a href="login" class="nav-link">Inscription</a>
+                        </li>
+                        <li class="text-white nav-item active">
+                            <a href="#" class="nav-link">Connexion</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <a class="btn d-lg-none" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                Catégories
+            </a>
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    <?php foreach ($category_list as $category) : ?>
+                        <p>
+                            <a href="/?category=<?= $category['slug'] ?>" role="button"><?= $category['name'] ?></a>
+                        </p>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+            <!--bouton hamburger-->
+            <nav class="navbar navbar-dark " id="bouttonMobile">
+                <button class="navbar-toggler bg-dark" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon" id="icon"></span>
+                </button>
+            </nav>
+            <!--fin menu mobile--->
+            <!--menu bureau-->
+            <div class="collapse navbar-collapse menu" id="navbarNav">
+                <ul class="navbar-nav  mx-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Accueil<span class="sr-only"></span></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse menu" id="navbarNav">
+                <ul class="navbar-nav  mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login">Connexion</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    <!--fin menu bureau--->
+    <!----FIN MENU---->
+    <!--BLOC BOUTON CATEGORIES bureau-->
+    <div class="container d-none d-lg-block d-xl-block">
+        <nav class="container navbar navbar-expand-lg ">
+            <ul class="navbar-nav mx-auto align-items-center">
+                <?php foreach ($category_list as $category) : ?>
+                    <li class="nav-item active p-2 m-2 bouton">
+                        <a class="nav-link btn" href="/?category=<?= $category['slug'] ?>" role="button"><?= $category['name'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
+    </div>
 
-    <form>
-        <div class="mb-3 col-4 ml">
-            <label class="form-label">Nom:</label>
-            <input type="name" class="form-control" id="pseudo">
-        </div>
-        <div class="mb-3 col-4">
-            <label class="form-label">Email</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        </div>
-        <div class="mb-3 col-4">
-            <label class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputEmail1">
-        </div>
-        <div class="mt-2 col-4">
-            <button type="submit" class="btn btn-primary">Se connecter</button>
-        </div>
-    </form>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <!--Contenu-->
+    <div class="container carte my-5"></div>
+        <form class="row justify-content-center flex-column g-5 bg-light p-2 rounded-lg">
+            <div class="mb-3 col-4 ml">
+                <label class="form-label">Nom:</label>
+                <input type="name" class="form-control" id="pseudo">
+            </div>
+            <div class="mb-3 col-4">
+                <label class="form-label">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3 col-4">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputEmail1">
+            </div>
+            <div class="mt-2 col-4">
+                <button type="submit" class="btn btn-primary">Se connecter</button>
+            </div>
+        </form>
+    </div>
+    <!---FOOTER---->
+    <div class="container-fluid footer p-2">
+        <ul class="navbar-nav text-center">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">DMCA<span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Contact<span class="sr-only"></span></a>
+            </li>
+        </ul>
+        <p class="text-center text-light ">Copyright</p>
+    </div>
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 
 </html>
